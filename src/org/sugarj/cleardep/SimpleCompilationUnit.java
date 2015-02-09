2625fe2ac29969eea5f5d143ec7ef1b25056bc15
Sebastian Erdweg <seba.github@gmail.com>
2015-02-09 14:08:46 +0100

    read simple CU

--- src/org/sugarj/cleardep/SimpleCompilationUnit.java
+++ src/org/sugarj/cleardep/SimpleCompilationUnit.java
@@ -3,7 +3,9 @@ package org.sugarj.cleardep;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Map;
 
+import org.sugarj.cleardep.stamp.Stamp;
 import org.sugarj.cleardep.stamp.Stamper;
 import org.sugarj.common.path.Path;
 
@@ -25,6 +27,14 @@ public class SimpleCompilationUnit extends CompilationUnit {
   protected void readEntity(ObjectInputStream ois) throws IOException, ClassNotFoundException {
     super.readEntity(ois);
     mode = (Mode<?>) ois.readObject();
+  }
+  
+  public static SimpleCompilationUnit readConsistent(Mode<SimpleCompilationUnit> mode, Map<? extends Path, Stamp> editedSourceFiles, Path... deps) throws IOException {
+    return CompilationUnit.readConsistent(SimpleCompilationUnit.class, mode, editedSourceFiles, deps);
+  }
+  
+  public static SimpleCompilationUnit read(Mode<SimpleCompilationUnit> mode, Path... deps) throws IOException {
+    return CompilationUnit.read(SimpleCompilationUnit.class, mode, deps);
   }
 
   public static SimpleCompilationUnit create(Stamper stamper, SimpleMode mode, Synthesizer syn, Path dep) throws IOException {
