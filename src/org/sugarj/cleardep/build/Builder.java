94cbfaf94fe8c6fdce1257f4e6f3b23abe38e988
Sebastian Erdweg <seba.github@gmail.com>
2015-02-09 15:50:13 +0100

    require as member

--- src/org/sugarj/cleardep/build/Builder.java
+++ src/org/sugarj/cleardep/build/Builder.java
@@ -14,32 +14,19 @@ public abstract class Builder<T, E extends CompilationUnit> {
     this.context = context;
   }
   
-  public abstract Class<E> resultClass();
-  public abstract Stamper defaultStamper();
-  public abstract void build(CompilationUnit result, T input);
+  protected abstract Class<E> resultClass();
+  protected abstract Stamper defaultStamper();
+  protected abstract void build(E result, T input);
   
-  
-  public <U, F extends CompilationUnit> CompilationUnit require(Builder<U, F> builder, U input, Path dep, Mode<F> mode) throws IOException {
-    CompilationUnit depResult = CompilationUnit.readConsistent(builder.resultClass(), mode, context.getEditedSourceFiles(), dep);
+  public CompilationUnit require(T input, Path dep, Mode<E> mode) throws IOException {
+    E depResult = CompilationUnit.readConsistent(resultClass(), mode, context.getEditedSourceFiles(), dep);
     if (depResult != null)
       return depResult;
     
-    depResult = CompilationUnit.create(builder.resultClass(), builder.defaultStamper(), mode, null, dep);
-    builder.build(depResult, input);
+    depResult = CompilationUnit.create(resultClass(), defaultStamper(), mode, null, dep);
+    build(depResult, input);
     
     return depResult;
   }
-  
-//  public <U, F extends CompilationUnit> CompilationUnit synthesize(Builder<U, F> builder, U input, Path dep, Mode<F> mode) throws IOException {
-//    CompilationUnit depResult = CompilationUnit.readConsistent(builder.resultClass(), mode, context.getEditedSourceFiles(), dep);
-//    if (depResult != null)
-//      return depResult;
-//    
-//    depResult = CompilationUnit.create(builder.resultClass(), builder.defaultStamper(), mode, null, dep);
-//    builder.build(depResult, input);
-//    
-//    return depResult;
-//  }
-//  
   
 }
