c6a9c36d57d779e741557cb728c0ec1cd0c7a7bc
Sebastian Erdweg <seba.github@gmail.com>
2015-02-09 14:51:32 +0100

    silent fail if xattr command not available

--- src/org/sugarj/cleardep/xattr/XattrCommandStrategy.java
+++ src/org/sugarj/cleardep/xattr/XattrCommandStrategy.java
@@ -13,29 +13,43 @@ public class XattrCommandStrategy implements XattrStrategy {
   
   @Override
   public void setXattr(Path p, String key, String value) throws IOException {
-    exec.execute("xattr", "-w", Xattr.PREFIX + ":" + key, value, p.getAbsolutePath());
+    try {
+      exec.execute("xattr", "-w", Xattr.PREFIX + ":" + key, value, p.getAbsolutePath());
+    } catch (CommandExecution.ExecutionError e) {
+      System.err.println(e.getMessage());
+    }
   }
 
   @Override
   public String getXattr(Path p, String key) throws IOException {
-    String[][] out = exec.execute("xattr", "-p", Xattr.PREFIX + ":" + key, p.getAbsolutePath());
-    if (out[0].length > 0)
-      return out[0][0];
-    return null;
+    try {
+      String[][] out = exec.execute("xattr", "-p", Xattr.PREFIX + ":" + key, p.getAbsolutePath());
+      if (out[0].length > 0)
+        return out[0][0];
+      return null;
+    } catch (CommandExecution.ExecutionError e) {
+      System.err.println(e.getMessage());
+      return null;
+    }
   }
 
   @Override
   public Map<String, String> getAllXattr(Path p) throws IOException {
-    String[][] out = exec.execute("xattr", "-l", p.getAbsolutePath());
-    
-    Map<String, String> attrs = new HashMap<>();
-    for (String line : out[0]) {
-      int split = line.indexOf(": ");
-      String key = line.substring(0, split);
-      String val = line.substring(split + ": ".length());
-      attrs.put(key, val);
+    try {
+      String[][] out = exec.execute("xattr", "-l", p.getAbsolutePath());
+      
+      Map<String, String> attrs = new HashMap<>();
+      for (String line : out[0]) {
+        int split = line.indexOf(": ");
+        String key = line.substring(0, split);
+        String val = line.substring(split + ": ".length());
+        attrs.put(key, val);
+      }
+      return attrs;
+    } catch (CommandExecution.ExecutionError e) {
+      System.err.println(e.getMessage());
+      return null;
     }
-    return attrs;
   }
 
 }
