39346f9225c5365b741062209d38b948ec7c65bd
Sebastian Erdweg <seba.github@gmail.com>
2015-02-11 08:55:45 +0100

    builder factory

---
+++ src/org/sugarj/cleardep/build/BuilderFactory.java
@@ -0,0 +1,7 @@
+package org.sugarj.cleardep.build;
+
+import org.sugarj.cleardep.CompilationUnit;
+
+public interface BuilderFactory<C extends BuildContext, T, E extends CompilationUnit, B extends Builder<C, T, E>> {
+  public B makeBuilder(C context);
+}
