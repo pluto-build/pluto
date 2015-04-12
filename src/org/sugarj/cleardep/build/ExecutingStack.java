a640d6541a11170c5fa2cd9cb9ba54dd306fd440
Moritz Lichter <lichtemo@googlemail.com>
2015-04-12 16:16:06 +0200

    Extracted merging of scc sets

--- src/org/sugarj/cleardep/build/ExecutingStack.java
+++ src/org/sugarj/cleardep/build/ExecutingStack.java
@@ -3,69 +3,37 @@ package org.sugarj.cleardep.build;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-
-import javax.print.attribute.standard.MediaSize.Other;
 
 import org.sugarj.cleardep.BuildUnit;
 import org.sugarj.cleardep.dependency.BuildRequirement;
+import org.sugarj.cleardep.util.UniteSets;
 import org.sugarj.common.FileCommands;
-import org.sugarj.common.path.Path;
 
 public class ExecutingStack {
 
   private List<BuildUnit<?>> requireCallStack = new ArrayList<>();
   
-  private Map<BuildUnit<?>, Set<BuildUnit<?>>> stronglyConnectedComponentsRepresentants = new HashMap<>();
-  private Map<BuildUnit<?>, BuildUnit<?>> stronglyConnectedComponentsMember = new HashMap<>();
-  
+  private UniteSets<BuildUnit<?>> sccs = new UniteSets<>();
   
 
   protected void push(BuildUnit<?> unit) throws IOException{
     int index = this.requireCallStack.indexOf(unit);
     if (index != -1) {
-      List<BuildRequirement<?>>  cycleComponents = new ArrayList<>();
-      BuildUnit<?> componentRepresentant = unit;
-      Set<BuildUnit<?>> scc = stronglyConnectedComponentsRepresentants.get(componentRepresentant);
-      
+      UniteSets<BuildUnit<?>>.Key scc = sccs.getOrCreateSet(unit);     
+      // All units on the cyclic stack area are in one scc, this may merge existing ones
       for (int i = index; scc != null && i < requireCallStack.size(); i ++) {
-        BuildUnit<?> cyclePath = requireCallStack.get(i);
-        BuildUnit<?> cyclePathSSCRep = stronglyConnectedComponentsMember.get(cyclePath);
-        if (cyclePathSSCRep != null) {
-          componentRepresentant = cyclePathSSCRep;
-          scc = stronglyConnectedComponentsRepresentants.get(componentRepresentant);
-        }
+        scc = sccs.uniteOrAdd(scc, requireCallStack.get(i));
       }
       
-      if (scc == null) {
-        scc = new HashSet<>();
-        scc.add(componentRepresentant);
-        stronglyConnectedComponentsRepresentants.put(componentRepresentant, scc);
+      // Get all elements of the scc
+      List<BuildRequirement<?>>  cycleComponents = new ArrayList<>();
+      for (BuildUnit<?> p : sccs.getSetMembers(scc)) {
+        cycleComponents.add(requirementForEntry(p));
       }
       
-      for (int i = index; i < requireCallStack.size(); i ++) {
-        BuildUnit<?> cyclePath = requireCallStack.get(i);
-        BuildUnit<?> cyclePathSSCRep = stronglyConnectedComponentsMember.get(cyclePath);
-        if (cyclePathSSCRep != componentRepresentant) {
-          if (cyclePathSSCRep != null) {
-            for (BuildUnit<?> otherSCCPart : stronglyConnectedComponentsRepresentants.get(cyclePathSSCRep)) {
-              stronglyConnectedComponentsMember.put(otherSCCPart, componentRepresentant);
-              scc.add(otherSCCPart);
-            }
-            stronglyConnectedComponentsRepresentants.remove(cyclePathSSCRep);
-          } else {
-            scc.add(cyclePath);
-            stronglyConnectedComponentsMember.put(cyclePath, componentRepresentant);
-          }
-        }
-      }
-      for (BuildUnit<?> p : scc) {
-      cycleComponents.add(requirementForEntry(p));
-      }
       BuildCycleException ex = new BuildCycleException("Build contains a dependency cycle on " + FileCommands.tryGetRelativePath(unit.getPersistentPath()), unit, cycleComponents);
      throw ex;
     }
