2b19320401cab7fd27ba4f5aa911a72f71445791
Sebastian Erdweg <seba.github@gmail.com>
2016-03-21 11:24:51 +0100

    no reporting adaptor

--- test/build/pluto/test/build/EnsureNoBuilderStartedReporting.java
+++ test/build/pluto/test/build/EnsureNoBuilderStartedReporting.java
@@ -4,81 +4,14 @@ import java.util.Set;
 
 import junit.framework.AssertionFailedError;
 import build.pluto.BuildUnit;
-import build.pluto.builder.BuildCycle;
-import build.pluto.builder.BuildCycleException;
 import build.pluto.builder.BuildRequest;
 import build.pluto.builder.Builder;
-import build.pluto.builder.CycleHandler;
-import build.pluto.builder.RequiredBuilderFailed;
-import build.pluto.dependency.Requirement;
 import build.pluto.output.Output;
-import build.pluto.util.IReporting;
+import build.pluto.util.NoReporting;
 
-public class EnsureNoBuilderStartedReporting implements IReporting {
-
-  @Override
-  public <O extends Output> void buildRequirement(BuildRequest<?, O, ?, ?> req) {
-  }
-
-  @Override
-  public <O extends Output> void finishedBuildRequirement(BuildRequest<?, O, ?, ?> req) {
-  }
-
+public class EnsureNoBuilderStartedReporting extends NoReporting {
   @Override
   public <O extends Output> void startedBuilder(BuildRequest<?, O, ?, ?> req, Builder<?, ?> b, BuildUnit<O> oldUnit, Set<BuildReason> reasons) {
     throw new AssertionFailedError("Builder was started " + req);
   }
-
-  @Override
-  public <O extends Output> void finishedBuilder(BuildRequest<?, O, ?, ?> req, BuildUnit<O> unit) {
-  }
-
-  @Override
-  public <O extends Output> void skippedBuilder(BuildRequest<?, O, ?, ?> req, BuildUnit<O> unit) {
-  }
-
-  @Override
-  public <O extends Output> void canceledBuilderFailure(BuildRequest<?, O, ?, ?> req, BuildUnit<O> unit) {
-  }
-
-  @Override
-  public <O extends Output> void canceledBuilderException(BuildRequest<?, O, ?, ?> req, BuildUnit<O> unit, Throwable t) {
-  }
-
-  @Override
-  public <O extends Output> void canceledBuilderCycle(BuildRequest<?, O, ?, ?> req, BuildUnit<O> unit, BuildCycleException t) {
-  }
-
-  @Override
-  public <O extends Output> void canceledBuilderInterrupt(BuildRequest<?, O, ?, ?> req, BuildUnit<O> unit) {
-  }
-
-  @Override
-  public <O extends Output> void canceledBuilderRequiredBuilderFailed(BuildRequest<?, O, ?, ?> req, BuildUnit<O> unit, RequiredBuilderFailed e) {
-  }
-
-  @Override
-  public void startBuildCycle(BuildCycle cycle, CycleHandler cycleSupport) {
-  }
-
-  @Override
-  public void finishedBuildCycle(BuildCycle cycle, CycleHandler cycleSupport, Set<BuildUnit<?>> units) {
-  }
-
-  @Override
-  public void cancelledBuildCycleException(BuildCycle cycle, CycleHandler cycleSupport, Throwable t) {
-  }
-
-  @Override
-  public void inconsistentRequirement(Requirement req) {
-  }
-
-  @Override
-  public void messageFromBuilder(String message, boolean isError, Builder<?, ?> from) {
-  }
-
-  @Override
-  public void messageFromSystem(String message, boolean isError, int verbosity) {
-  }
-
 }
