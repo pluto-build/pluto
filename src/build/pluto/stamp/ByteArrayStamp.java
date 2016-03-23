24161444afad7e097f1290a53779893e2867bcd0
Gabriel Konat <gabrielkonat@gmail.com>
2016-03-23 18:23:44 +0100

    Add missing equals(Object) methods for ValueStamp and ByteArrayStamp, such that object equality is used instead of reference equality when comparing nested stamps (such as a Map of Stamps).

--- src/build/pluto/stamp/ByteArrayStamp.java
+++ src/build/pluto/stamp/ByteArrayStamp.java
@@ -4,23 +4,39 @@ import java.util.Arrays;
 
 public class ByteArrayStamp implements Stamp {
   private static final long serialVersionUID = 3600975022352030761L;
-  
+
   public final Stamper stamper;
   public final byte[] val;
-  
+
   public ByteArrayStamp(Stamper stamper, byte[] val) {
     this.stamper = stamper;
     this.val = val;
   }
-  
+
   @Override
   public Stamper getStamper() {
     return stamper;
   }
-  
+
   @Override
-  public boolean equals(Stamp o) {
-    return o instanceof ByteArrayStamp && Arrays.equals(val, ((ByteArrayStamp) o).val);
+  public boolean equals(Stamp obj) {
+    return equals((Object) obj);
   }
 
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    final ByteArrayStamp other = (ByteArrayStamp) obj;
+    return Arrays.equals(val, other.val);
+  }
+
+  @Override
+  public int hashCode() {
+    return Arrays.hashCode(val);
+  }
 }
